d19a3c86f102f63e7476b472fa85dc98eb5c9586
Faisal Jamil <jamilfaisal43@gmail.com>
2025-08-07 22:07:01 -0400

    Update .gitignore

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -194,7 +194,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -204,11 +204,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres.einfbaempyvmaktermnt:MkZWxGYJwSWh3UH8@aws-0-us-east-2.pooler.supabase.com:5432/postgres"
+        "value": null
       }
     }
   },
